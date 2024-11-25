dff8940aace550324e16f5321da28a1f482eb4b3
JulianoL13 <julianov1383@gmail.com>
2024-11-24 21:19:20 -0300

    feat: implement auth

--- src/routes/userRoutes/resourceRoutes.ts
+++ src/routes/userRoutes/resourceRoutes.ts
@@ -2,16 +2,26 @@ import { Router } from "express";
 import { ResourceRepository } from "../../repositories/userRepository/resourceRepository";
 import { ResourceService } from "../../services/userService/resourceService";
 import { ResourceController } from "../../controllers/userController/resourceController";
+import { AuthMiddleware } from "../../middleware/authMiddleware";
 
 const resourceRepository = new ResourceRepository();
 const resourceService = new ResourceService(resourceRepository);
 const resourceController = new ResourceController(resourceService);
+const authMiddleware = new AuthMiddleware();
 
 const router = Router();
 
-router.get("/resource", resourceController.getResources);
-router.get("/resource/:id", resourceController.getResourceById);
-router.post("/resource", resourceController.createResource);
-router.delete("/resource/:id", resourceController.deleteResource);
+router.get("/resource", authMiddleware.authenticateToken, (req, res) =>
+  resourceController.getResources(req, res),
+);
+router.get("/resource/:id", authMiddleware.authenticateToken, (req, res) =>
+  resourceController.getResourceById(req, res),
+);
+router.post("/resource", authMiddleware.authenticateToken, (req, res) =>
+  resourceController.createResource(req, res),
+);
+router.delete("/resource/:id", authMiddleware.authenticateToken, (req, res) =>
+  resourceController.deleteResource(req, res),
+);
 
 export default router;
--- src/routes/userRoutes/userRoutes.ts
+++ src/routes/userRoutes/userRoutes.ts
@@ -3,29 +3,41 @@ import { UserController } from "../../controllers/userController/userController"
 import { UserService } from "../../services/userService/userService";
 import { UserRepository } from "../../repositories/userRepository/userRepository";
 import { DocumentsUtils } from "../../utils/documentsUtil";
+import { AuthMiddleware } from "../../middleware/authMiddleware";
 
 const documentsUtil = new DocumentsUtils();
 const userRepository = new UserRepository();
 const userService = new UserService(userRepository, documentsUtil);
 const userController = new UserController(userService);
+const authMiddleware = new AuthMiddleware();
 const router = Router();
 
-router.get("/users", (req, res) => userController.getUsers(req, res));
-router.get("/users/cpf/:cpf", (req, res) =>
+router.get("/users", authMiddleware.authenticateToken, (req, res) =>
+  userController.getUsers(req, res),
+);
+router.get("/users/cpf/:cpf", authMiddleware.authenticateToken, (req, res) =>
   userController.getUserByCpf(req, res),
 );
-router.get("/users/email/:email", (req, res) =>
-  userController.getUserByEmail(req, res),
+router.get(
+  "/users/email/:email",
+  authMiddleware.authenticateToken,
+  (req, res) => userController.getUserByEmail(req, res),
 );
-router.get("/users/registration/:registration", (req, res) =>
-  userController.getUserByRegistration(req, res),
+router.get(
+  "/users/registration/:registration",
+  authMiddleware.authenticateToken,
+  (req, res) => userController.getUserByRegistration(req, res),
 );
-router.get("/users/:userId", (req, res) =>
+router.get("/users/:userId", authMiddleware.authenticateToken, (req, res) =>
   userController.getUserById(req, res),
 );
-router.post("/users", (req, res) => userController.createUser(req, res));
-router.put("/users/:userId", (req, res) => userController.updateUser(req, res));
-router.delete("/users/:userId", (req, res) =>
+router.post("/users", authMiddleware.authenticateToken, (req, res) =>
+  userController.createUser(req, res),
+);
+router.put("/users/:userId", authMiddleware.authenticateToken, (req, res) =>
+  userController.updateUser(req, res),
+);
+router.delete("/users/:userId", authMiddleware.authenticateToken, (req, res) =>
   userController.deleteUser(req, res),
 );
 
